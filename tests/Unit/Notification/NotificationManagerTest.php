a3cd2620e447b35554525514bfff334b1f7f0a84
Mikhail Ilinsky <mail@milinsky.ru>
2026-05-21 05:21:00 +1000

    test: rewrite NotificationManager tests with mock SocketPair
    Replace real socket_create_pair with mock NotificationSocketPairInterface.
    11 tests cover all branches: enable/disable/notify/reset/exception propagation.
    100% coverage achieved. PSR-3 mock logger verifies logging.

--- tests/Unit/Notification/NotificationManagerTest.php
+++ tests/Unit/Notification/NotificationManagerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Duyler\HttpServer\Tests\Unit\Notification;
 
+use Duyler\HttpServer\Exception\SocketException;
 use Duyler\HttpServer\Notification\NotificationManager;
 use Duyler\HttpServer\Socket\NotificationSocketPairInterface;
 use Override;
@@ -11,7 +12,7 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Psr\Log\NullLogger;
+use Psr\Log\LoggerInterface;
 
 #[CoversClass(NotificationManager::class)]
 class NotificationManagerTest extends TestCase
@@ -25,13 +26,13 @@ class NotificationManagerTest extends TestCase
     {
         parent::setUp();
         $this->socketPair = $this->createMock(NotificationSocketPairInterface::class);
-        $this->manager = new NotificationManager($this->socketPair, new NullLogger());
+        $this->manager = new NotificationManager($this->socketPair);
     }
 
     #[Test]
     public function enable_creates_pair_when_not_enabled(): void
     {
-        $this->socketPair->method('isEnabled')->willReturn(false);
+        $this->socketPair->expects($this->once())->method('isEnabled')->willReturn(false);
         $this->socketPair->expects($this->once())->method('createPair');
 
         $this->manager->enable();
@@ -40,8 +41,21 @@ class NotificationManagerTest extends TestCase
     #[Test]
     public function enable_skips_when_already_enabled(): void
     {
-        $this->socketPair->method('isEnabled')->willReturn(true);
+        $this->socketPair->expects($this->once())->method('isEnabled')->willReturn(true);
         $this->socketPair->expects($this->never())->method('createPair');
+
+        $this->manager->enable();
+    }
+
+    #[Test]
+    public function enable_propagates_exception_from_create_pair(): void
+    {
+        $exception = SocketException::fromLastError();
+
+        $this->socketPair->expects($this->once())->method('isEnabled')->willReturn(false);
+        $this->socketPair->expects($this->once())->method('createPair')->willThrowException($exception);
+
+        $this->expectException(SocketException::class);
 
         $this->manager->enable();
     }
@@ -55,9 +69,21 @@ class NotificationManagerTest extends TestCase
     }
 
     #[Test]
+    public function disable_logs_info_message(): void
+    {
+        $this->socketPair->expects($this->once())->method('close');
+
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->once())->method('info')->with('Notification sockets disabled');
+
+        $manager = new NotificationManager($this->socketPair, $logger);
+        $manager->disable();
+    }
+
+    #[Test]
     public function is_enabled_delegates_to_socket_pair(): void
     {
-        $this->socketPair->method('isEnabled')->willReturn(true);
+        $this->socketPair->expects($this->once())->method('isEnabled')->willReturn(true);
 
         $this->assertTrue($this->manager->isEnabled());
     }
@@ -65,57 +91,23 @@ class NotificationManagerTest extends TestCase
     #[Test]
     public function is_enabled_returns_false_when_pair_disabled(): void
     {
-        $this->socketPair->method('isEnabled')->willReturn(false);
+        $this->socketPair->expects($this->once())->method('isEnabled')->willReturn(false);
 
         $this->assertFalse($this->manager->isEnabled());
     }
 
     #[Test]
-    public function get_read_socket_returns_socket_from_pair(): void
-    {
-        $sockets = [];
-        socket_create_pair(AF_UNIX, SOCK_STREAM, 0, $sockets);
-        $realSocket = $sockets[0];
-
-        $this->socketPair->method('getReadSocket')->willReturn($realSocket);
-
-        $result = $this->manager->getReadSocket();
-
-        $this->assertSame($realSocket, $result);
-
-        socket_close($sockets[0]);
-        socket_close($sockets[1]);
-    }
-
-    #[Test]
     public function get_read_socket_returns_null_when_no_pair(): void
     {
-        $this->socketPair->method('getReadSocket')->willReturn(null);
+        $this->socketPair->expects($this->once())->method('getReadSocket')->willReturn(null);
 
         $this->assertNull($this->manager->getReadSocket());
     }
 
     #[Test]
-    public function get_notify_socket_returns_write_socket(): void
-    {
-        $sockets = [];
-        socket_create_pair(AF_UNIX, SOCK_STREAM, 0, $sockets);
-        $realSocket = $sockets[1];
-
-        $this->socketPair->method('getWriteSocket')->willReturn($realSocket);
-
-        $result = $this->manager->getNotifySocket();
-
-        $this->assertSame($realSocket, $result);
-
-        socket_close($sockets[0]);
-        socket_close($sockets[1]);
-    }
-
-    #[Test]
     public function get_notify_socket_returns_null_when_no_pair(): void
     {
-        $this->socketPair->method('getWriteSocket')->willReturn(null);
+        $this->socketPair->expects($this->once())->method('getWriteSocket')->willReturn(null);
 
         $this->assertNull($this->manager->getNotifySocket());
     }
@@ -134,11 +126,5 @@ class NotificationManagerTest extends TestCase
         $this->socketPair->expects($this->once())->method('close');
 
         $this->manager->reset();
-    }
-
-    #[Test]
-    public function set_notify_socket_does_not_exist(): void
-    {
-        $this->assertFalse(method_exists($this->manager, 'setNotifySocket'));
     }
 }
